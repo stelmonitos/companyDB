2af3c0639e900616a1e1cfbcbd93f588c5458481
Jakub Stelmasiak <stelmon3@gmail.com>
2024-06-05 21:45:31 +0200

    connection issue

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const mongoClient = require('mongodb').MongoClient;
-const dbData = require('./db'); // Import danych z db.js
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
-mongoClient.connect('mongodb://0.0.0.0:27017', (err, client) => {
-  if (err) {
+mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if(err) {
     console.log(err);
-  } else {
+  }
+  else {
     console.log('Successfully connected to the database');
     const db = client.db('companyDB');
-
-    // Import danych do MongoDB
-
-    db.collection('departments')
-      .insertOne({ name: 'Management' })
-      .catch((err) => {
-        console.log(err);
-    });
-
     db.collection('employees')
-    .find({ department: 'IT' })
-    .toArray()
-    .then((item) => {
-      console.log(item);
-     })
-     .catch((err) => {
-       console.log(err);
-     });
+  .find({ department: 'IT' })
+  .toArray()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
     const app = express();
 
@@ -44,7 +35,7 @@ mongoClient.connect('mongodb://0.0.0.0:27017', (err, client) => {
 
     app.use((req, res) => {
       res.status(404).send({ message: 'Not found...' });
-    });
+    })
 
     app.listen('8000', () => {
       console.log('Server is running on port: 8000');
