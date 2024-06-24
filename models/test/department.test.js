3e8e04839e8a4b7f966282808b2603ca0e4f50ac
Jakub Stelmasiak <stelmon3@gmail.com>
2024-06-24 09:46:40 +0200

    validation issue

---
+++ models/test/department.test.js
@@ -0,0 +1,56 @@
+const Department = require('../department.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+
+describe('Department', () => {
+    
+    it('should throw an error if no "name" arg', () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+        
+        dep.validateSync(err => {
+            expect(err.errors.name).to.exist;
+        });
+    });
+    it('should throw an error if "name" is not a string', () => {
+        
+        const cases = [{}, []];
+        for (let name of cases) {
+            const dep = new Department({ name });
+            
+            dep.validateSync(err => {
+                expect(err.errors.name).to.exist;
+            });
+        }
+    });
+    it('should throw an error if "name" is too short or too long', () => {
+        
+        // const cases = ['Abc', 'abcd', 'Lorem Ipsum, Lorem Ip']; // we test various cases, some of them are too short, some of them are too long
+        const cases = ['Abcdef']
+
+        for(let name of cases) {
+            const dep = new Department({ name });
+            const error = dep.validateSync();
+            console.log(error);
+            expect(error.errors.name).to.exist;
+            
+        }
+        
+    });
+    it('should not throw an error if "name" is okay', () => {
+        
+        const cases = ['Management', 'Human Resources'];
+        for(let name of cases) {
+            const dep = new Department({ name });
+            
+            dep.validateSync(err => {
+                expect(err).to.not.exist;
+            });
+            
+        }
+    });
+    
+    after(() => {
+        mongoose.models = {};
+    });
+});
---
+++ models/test/employees.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../employees.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+
+    it('should throw an error if no arg', () => {
+        const emp = new Employee({})
+
+        emp.validateSync(err => {
+            expect(err.errors.name).to.exist
+        })
+    });
+    it('should throw an error if arg is not a string', () => {
+        const cases = [{}, []];
+        for (let arg of cases) {
+            const emp = new Employee({ firstName: arg, lastName: arg, department: arg });
+            const errors = emp.validateSync();
+            expect(errors.errors.name).to.exist;
+        };
+    });
+    // it('should throw an error if "name" is too short or too long', () => {
+
+    // });
+    // it('should not throw an error if "name" is okay', () => {
+
+    // });
+    after(() => {
+        mongoose.models = {};
+    });
+})
